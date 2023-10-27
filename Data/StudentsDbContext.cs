9480136497445153884f5b1621919a09a2e6daa0
Oscar Santamaria <oscarsm16@outlook.com>
2023-10-27 14:11:09 +0300

    Add project files.

---
+++ Data/StudentsDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class StudentsDbContext : DbContext
+    {
+        public StudentsDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Yardi\source\repos\Project\Data\StudentsDb.mdf;Integrated Security=True");
+        }
+    }
+}
